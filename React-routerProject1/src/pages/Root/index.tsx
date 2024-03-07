4ffd564539c6fb033d201d8ee0915b14ff228594
Vova <bereznikovvova1@yandex.ru>
2024-03-07 13:11:50 +0300

    [add] поменял некоторые стили добавил локальное хранилище для темы

--- React-routerProject1/src/pages/Root/index.tsx
+++ React-routerProject1/src/pages/Root/index.tsx
@@ -1,17 +1,32 @@
 import useContactsData from "../../hooks/useContactsData";
 import { useState } from "react";
 import "./Root.css";
-import { Link, Outlet } from "react-router-dom";
+import { Form, NavLink, Outlet } from "react-router-dom";
+
+// export function action() {
+//   const contact = createContact();
+//   console.log(contact);
+//   return { contact };
+// }
+
 const Root = () => {
   const { data, isLoading } = useContactsData();
   const [filterWord, setFilterWord] = useState<string>("");
   const DarkMode = () => {
     document.querySelector("body")?.setAttribute("data-theme", "dark");
+    localStorage.setItem("selectedTheme", "dark");
   };
   const LightMode = () => {
     document.querySelector("body")?.setAttribute("data-theme", "light");
+    localStorage.setItem("selectedTheme", "light");
   };
-  const ToggleMode = (e) => {
+  const theme = localStorage.getItem("selectedTheme");
+  if (theme === "dark") {
+    DarkMode();
+  } else {
+    LightMode();
+  }
+  const ToggleMode = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.checked) {
       DarkMode();
     } else {
@@ -30,6 +45,7 @@ const Root = () => {
               type="checkbox"
               onChange={ToggleMode}
               className="switch__input"
+              defaultChecked={theme === "dark" ? true : false}
             />
             <span className="switch__slider"></span>
           </label>
@@ -46,16 +62,22 @@ const Root = () => {
                 : el.name.toLowerCase().includes(filterWord.toLowerCase()),
             )
             .map((contact, index) => (
-              <Link
-                className="contact-link"
+              <NavLink
                 key={index}
                 to={`/contact/${index}`}
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "contact-link__active"
+                    : isPending
+                      ? "contact-link__pending"
+                      : "contact-link"
+                }
               >
                 <div className="avatar">
                   <img src={contact.avatarURL} />
                 </div>
                 {contact.name}
-              </Link>
+              </NavLink>
             ))}
         </div>
       )}
